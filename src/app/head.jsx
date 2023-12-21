12dd14572a95d62979cd46a9403f74440341c4bf
Abhayraj262 <abhayrajkashyap2002@gmail.com>
2023-12-22 02:28:49 +0530

    install  next  js  and CREATE the first template

---
+++ src/app/head.jsx
@@ -0,0 +1,10 @@
+export default function Head() {
+    return (
+      <>
+        <title>Google clone Next js 13 </title>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="description" content="Google clone create by Next js 13" />
+        <link rel="icon" href="/favicon.ico" />
+      </>
+    )
+  }
